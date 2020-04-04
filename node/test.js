3284a08ab85e36be779329d5c2a672c2fb431735
Michael Yuan <michael@michaelyuan.com>
2020-04-04 17:42:00 -0500

    Give credits

--- node/test.js
+++ node/test.js
@@ -1,8 +1,8 @@
 const { say, obfusticate, lowest_common_denominator, sha3_digest, keccak_digest, create_line } = require('./ssvm_nodejs_starter_lib.js');
 
-var util = require('util');
-const encoder = new util.TextEncoder();
-
+// Utils
+const encoder = new TextEncoder();
+// From https://nielsleenheer.com/articles/2017/the-case-for-console-hex/
 console.hex = (d) => console.log((Object(d).buffer instanceof ArrayBuffer ? new Uint8Array(d.buffer) : typeof d === 'string' ? (new TextEncoder('utf-8')).encode(d) : new Uint8ClampedArray(d)).reduce((p, c, i, a) => p + (i % 16 === 0 ? i.toString(16).padStart(6, 0) + '  ' : ' ') + c.toString(16).padStart(2, 0) + (i === a.length - 1 || i % 16 === 15 ?  ' '.repeat((15 - i % 16) * 3) + Array.from(a).splice(i - i % 16, 16).reduce((r, v) => r + (v > 31 && v < 127 || v > 159 ? String.fromCharCode(v) : '.'), '  ') + '\n' : ''), ''));
 
 console.log( say("SSVM") );
