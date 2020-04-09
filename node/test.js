800aeadd6aa69b62019a1f2ee1d1ea1707711b00
Michael Yuan <michael@michaelyuan.com>
2020-04-09 12:34:59 -0500

    Update test.js

--- node/test.js
+++ node/test.js
@@ -7,7 +7,7 @@ const encoder = new util.TextEncoder();
 console.hex = (d) => console.log((Object(d).buffer instanceof ArrayBuffer ? new Uint8Array(d.buffer) : typeof d === 'string' ? (new util.TextEncoder('utf-8')).encode(d) : new Uint8ClampedArray(d)).reduce((p, c, i, a) => p + (i % 16 === 0 ? i.toString(16).padStart(6, 0) + '  ' : ' ') + c.toString(16).padStart(2, 0) + (i === a.length - 1 || i % 16 === 15 ?  ' '.repeat((15 - i % 16) * 3) + Array.from(a).splice(i - i % 16, 16).reduce((r, v) => r + (v > 31 && v < 127 || v > 159 ? String.fromCharCode(v) : '.'), '  ') + '\n' : ''), ''));
 
 console.log( say("SSVM") );
-// console.log( obfusticate("A quick brown fox jumps over the lazy dog") );
+console.log( obfusticate("A quick brown fox jumps over the lazy dog") );
 console.log( lowest_common_denominator(123, 2) );
 console.hex( sha3_digest(encoder.encode("This is an important message")) );
 console.hex( keccak_digest(encoder.encode("This is an important message")) );
