b3e3878a884ab873685a7d7529cd47bcc1597b5b
Michael Yuan <michael@michaelyuan.com>
2020-05-08 21:20:14 -0500

    Update app.js

--- node/app.js
+++ node/app.js
@@ -3,7 +3,7 @@ const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
 const http = require('http');
 const url = require('url');
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
