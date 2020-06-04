4b6b827da5bbe9a4deda08defbb8502f2cb31be5
Michael Yuan <michael@secondstate.io>
2020-06-04 22:06:59 +0000

    use println in rust

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
+  println!("The Rust function say() received {}", s);
   let r = String::from("hello ");
   return r + s;
 }
