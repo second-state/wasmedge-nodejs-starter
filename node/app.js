6862cacaa3c58160c637a8da88412637779bbcf5
Linden Chiu <hchiu87@gmail.com>
2020-08-16 11:17:02 -0700

    partial working fibonacci

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { fibonacci, say } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
 const http = require('http');
 const url = require('url');
@@ -7,11 +7,17 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
+  const num = parseInt(queryObject['number'], 10
+  )
+  console.log('testing', fibonacci(num))
+
+  res.end()
+
+  // if (!queryObject['name']) {
+  //   res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+  // } else {
+  //   res.end(say(queryObject['name']) + '\n');
+  // }
 });
 
 server.listen(port, hostname, () => {
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,12 @@ pub fn say(s: &str) -> String {
   let r = String::from("hello ");
   return r + s;
 }
+
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> u32 {
+  match n {
+      0 => 1,
+      1 => 1,
+      _ => fibonacci(n - 1) + fibonacci(n - 2),
+  }
+}
