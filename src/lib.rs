940eb38d7f2f537beb27e9c4b11309a281d476a7
Linden Chiu <hchiu87@gmail.com>
2020-08-18 01:33:08 -0700

    refactor fibo

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,8 @@ use num_traits::{Zero, One};
 #[wasm_bindgen]
 pub fn fibonacci(n: u32) -> u32 {
   match n {
-      0 => 1,
-      1 => 1,
-      _ => fibonacci(n - 1) + fibonacci(n - 2),
+    0 | 1 => n,
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
   }
 }
 
