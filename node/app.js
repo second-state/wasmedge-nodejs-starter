8da325128edf18b48cb5b93c97ee5f42b44675cb
Linden Chiu <hchiu87@gmail.com>
2020-08-18 00:43:14 -0700

    working web interface

--- node/app.js
+++ node/app.js
@@ -1,25 +1,38 @@
+const express = require('express');
 const { fibonacci, is_prime } = require('../pkg/ssvm_nodejs_starter_lib.js');
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
+// const http = require('http');
+// const url = require('url');
+// const hostname = '0.0.0.0';
+const app = express();
 const port = 3000;
+const hostname = '0.0.0.0';
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  const num = parseInt(queryObject['number'], 10
-  )
-  console.log('testing', is_prime(num))
+// const server = http.createServer((req, res) => {
+//   const queryObject = url.parse(req.url,true).query;
+//   const num = parseInt(queryObject['number'], 10
+//   )
+//   console.log('testing', is_prime(num))
 
-  res.end()
+//   res.end()
 
-  // if (!queryObject['name']) {
-  //   res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  // } else {
-  //   res.end(say(queryObject['name']) + '\n');
-  // }
-});
+//   // if (!queryObject['name']) {
+//   //   res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+//   // } else {
+//   //   res.end(say(queryObject['name']) + '\n');
+//   // }
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', (req, res) => res.redirect("/index.html"));
+
+app.post('/is_prime', function (req, res) {
+  let a = parseInt(req.body.num);
+  //let result = null;
+ 
+  res.send(is_prime(a));
+})
+
+app.listen(port, () => 
+  console.log(`Server running at http://localhost:${port}/`));
