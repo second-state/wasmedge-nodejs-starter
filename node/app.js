5aea51e0427c7628624fd72cbd3e67818c184b4c
Linden Chiu <hchiu87@gmail.com>
2020-08-20 23:44:13 -0700

    working state

--- node/app.js
+++ node/app.js
@@ -8,28 +8,12 @@ const app = express();
 import pkg from '../pkg/ssvm_nodejs_starter_lib.js';
 const { fibonacci, is_prime } = pkg;
 
-// const http = require('http');
-// const url = require('url');
-// const hostname = '0.0.0.0';
+
 const port = 3000;
 const hostname = '0.0.0.0';
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
-// const server = http.createServer((req, res) => {
-//   const queryObject = url.parse(req.url,true).query;
-//   const num = parseInt(queryObject['number'], 10
-//   )
-//   console.log('testing', is_prime(num))
-
-//   res.end()
-
-//   // if (!queryObject['name']) {
-//   //   res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-//   // } else {
-//   //   res.end(say(queryObject['name']) + '\n');
-//   // }
-// });
 
 app.get('/', (req, res) => res.redirect("/index.html"));
 
