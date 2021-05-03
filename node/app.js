3ef97c943155b51b2195e9504e4109f57e5df11f
Michael Yuan <michael@secondstate.io>
2021-05-04 03:09:04 +0800

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { say } = require('../pkg/wasmedge_nodejs_starter_lib.js');
 
 const http = require('http');
 const url = require('url');
